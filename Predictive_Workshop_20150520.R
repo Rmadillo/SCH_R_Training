ecf991fbc102e08a193d6d24aa0f2d291de6aa80
Rmadillo <Rmadillo@users.noreply.github.com>
2016-05-17 13:40:49 -0700

    Update Predictive_Workshop_20150520.R

--- Predictive_Workshop_20150520.R
+++ Predictive_Workshop_20150520.R
@@ -1,3 +1,19 @@
+#install.packages(c("RGtk2", "magrittr", "stringi", "pmml", "bitops", "colorspace", "ada", "amap", "arules", "arulesViz", "biclust", "cairoDevice", "cba", "cluster", "corrplot", "descr", "doBy", "dplyr", "e1071", "ellipse", "fBasics", "foreign", "fpc", "gdata", "ggdendro", "ggplot2", "gplots", "gridExtra", "gtools", "gWidgetsRGtk2", "hmeasure", "Hmisc", "kernlab", "Matrix", "mice", "nnet", "odfWeave", "party", "playwith", "plyr", "psych", "randomForest", "RColorBrewer", "readxl", "reshape", "rggobi", "RGtk2Extras", "ROCR", "RODBC", "rpart", "rpart.plot", "SnowballC", "stringr", "survival", "timeDate", "tm", "verification", "wskm", "XML"))
 
-# install.packages("rattle", dep=c("Suggests"))
+# install.packages("rattle")
+# library(rattle)
+
+# If the RGtk2 package is not installed, there will be an error popup indicating that 
+# libatk-1.0-0.dll is missing from your computer. Click OK and then you will be asked 
+# if you would like to install GTK+. Click OK. This then downloads and installs the 
+# appropriate GTK+ libraries for your computer. After this has finished, exit from R and 
+# restart it so that it can find the newly installed libraries.
+
+library(rattle)
+rattle()
+
+
+
+
+
 
